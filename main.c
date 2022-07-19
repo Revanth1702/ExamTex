9e6dcec88ab99a92ffb46ed7f6e7eb929c847451
Revanth1702 <revanth.talluri@students.iiit.ac.in>
2022-07-19 13:05:49 +0400

    update main.c

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
-#include "adt.h"
+#include "ast.h"
 
 int main(void)
 {
-    FILE*q=fopen("qp.txt","r");
+
+  //  FILE *Nfp = fopen("questionpaper.txt", "w"); //appends i.e,creates questionbank.txt filegcc 
+    FILE *fp = fopen("questionbank.txt", "r");
+    FILE *q=fopen("qp.txt","r");
+
     Parseqp(q);
+
+    PtrToNode Root = ParseBank(fp); // Parses the data into an abstarct syntax tre
+
+    MakeQuesPaperFile(Root);
+
+    return 0;
 }
